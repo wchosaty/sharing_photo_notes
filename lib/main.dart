e5e3301172824781938adbfcf2f48a573175f625
W.Allen <whosaty@gmail.com>
2023-05-26 16:48:47 +0800

    新增動畫畫面進度備份

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:sharing_photo_notes/pages/browse_page.dart';
 import 'package:sharing_photo_notes/pages/edit_page.dart';
 import 'package:sharing_photo_notes/pages/login_page.dart';
 import 'package:sharing_photo_notes/pages/personal_page.dart';
+import 'package:sharing_photo_notes/pages/welcome_page.dart';
 import 'package:sharing_photo_notes/utils/log_data.dart';
 
 void main() {
@@ -29,91 +30,91 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         resizeToAvoidBottomInset: false,
         body: SafeArea(
-          child: HomePage(),
+          child: WelcomePage(),
         ),
       ),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+// class HomePage extends StatefulWidget {
+//   const HomePage({Key? key}) : super(key: key);
+//
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  static const tag = 'tag _HomePageState';
-  late Widget currentPage;
-  late SharedPreferences preferences;
-  int _currentIndex = 0;
-  final pages = [LoginPage(),BrowsePage(),PersonalPage()];
-
-  @override
-  void initState() {
-    super.initState();
-    currentPage = LoginPage();
-    loadUser();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      /// BottomNavigationBar
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.logout),label: 'logout'),
-        BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Browse'),
-        BottomNavigationBarItem(icon: Icon(Icons.edit_document),label: 'Edit'),
-      ],
-        currentIndex: _currentIndex,
-        onTap: _onItemClick,
-      ),
-      body: SafeArea(
-        child: Container(
-          /// BottomNavigationBar
-          child: pages[_currentIndex],
-
-          // child: LoginPage(),
-        ),
-      ),
-    );
-  }
-  Future loadUser() async {
-    preferences = await SharedPreferences.getInstance();
-    String username = preferences.getString("username") ?? "";
-    String nickname = preferences.getString("nickname") ?? "";
-    String token = preferences.getString("token") ?? "";
-    bool login = preferences.getBool("login") ?? false;
-
-    if (checkString(username) &&
-        checkString(nickname) &&
-        checkString(token) &&
-        login) {
-      LogData().d(tag, '已登入');
-      /// pushReplacementNamed 會清除push的stack頁面 /pushRepNamed 不會
-      // Navigator.of(context).pushReplacementNamed('/Personal', arguments: username );
-      MyApp.localUser = username.trim();
-      setState(() {
-        _currentIndex = 2;
-      });
-    } else {
-      LogData().d(tag, '無使用者');
-    }
-
-  }
-  bool checkString(String checkString) {
-    bool returnVal = false;
-    String s = checkString.trim();
-    if (s.isNotEmpty) returnVal = true;
-    return returnVal;
-  }
-
-  void _onItemClick(int value) {
-    setState(() {
-      _currentIndex = value;
-    });
-  }
-}
+// class _HomePageState extends State<HomePage> {
+//   static const tag = 'tag _HomePageState';
+//   late Widget currentPage;
+//   late SharedPreferences preferences;
+//   int _currentIndex = 0;
+//   final pages = [LoginPage(),BrowsePage(),PersonalPage()];
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     currentPage = LoginPage();
+//     loadUser();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       resizeToAvoidBottomInset: false,
+//       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+//       /// BottomNavigationBar
+//       bottomNavigationBar: BottomNavigationBar(items: [
+//         BottomNavigationBarItem(icon: Icon(Icons.logout),label: 'logout'),
+//         BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Browse'),
+//         BottomNavigationBarItem(icon: Icon(Icons.edit_document),label: 'Edit'),
+//       ],
+//         currentIndex: _currentIndex,
+//         onTap: _onItemClick,
+//       ),
+//       body: SafeArea(
+//         child: Container(
+//           /// BottomNavigationBar
+//           child: pages[_currentIndex],
+//
+//           // child: LoginPage(),
+//         ),
+//       ),
+//     );
+//   }
+//   Future loadUser() async {
+//     preferences = await SharedPreferences.getInstance();
+//     String username = preferences.getString("username") ?? "";
+//     String nickname = preferences.getString("nickname") ?? "";
+//     String token = preferences.getString("token") ?? "";
+//     bool login = preferences.getBool("login") ?? false;
+//
+//     if (checkString(username) &&
+//         checkString(nickname) &&
+//         checkString(token) &&
+//         login) {
+//       LogData().d(tag, '已登入');
+//       /// pushReplacementNamed 會清除push的stack頁面 /pushRepNamed 不會
+//       // Navigator.of(context).pushReplacementNamed('/Personal', arguments: username );
+//       MyApp.localUser = username.trim();
+//       setState(() {
+//         _currentIndex = 2;
+//       });
+//     } else {
+//       LogData().d(tag, '無使用者');
+//     }
+//
+//   }
+//   bool checkString(String checkString) {
+//     bool returnVal = false;
+//     String s = checkString.trim();
+//     if (s.isNotEmpty) returnVal = true;
+//     return returnVal;
+//   }
+//
+//   void _onItemClick(int value) {
+//     setState(() {
+//       _currentIndex = value;
+//     });
+//   }
+// }
